30f463b17bc0c87518e8d17b1bf6e4399908b2d7
fqds <121273hvv@gmail.com>
2022-12-28 07:41:20 +0300

    added another thread that gonna update googlesheets

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 # app.py
-import json
 from urllib import parse
 from flask import Flask, request
 
 app = Flask(__name__)
 
+from datetime import datetime
+from threading import Thread
+import time
+
 @app.route('/createdeal', methods=['POST'])
-def webhook():
-    if request.method == 'POST':
-        print(parse.parse_qs(request.get_data()))
-        return 'success', 200
-    else:
-        abort(400)
+def createDealHook():
+    data = parse.parse_qs(request.get_data())
+    
+    dealList.append({
+                        "moveAt":  int(data[b'leads[add][0][date_create]'][0].decode("utf-8")), 
+                        "dealID": data[b'leads[add][0][id]'][0].decode("utf-8"), 
+                        "name": data[b'leads[add][0][name]'][0].decode("utf-8"),
+                        "price":  data[b'leads[add][0][price]'][0].decode("utf-8"),
+                        "tableCoord": [len(dealList)+1, "A"]
+                    }) 
+    print(dealList)
+    return 'success', 200
+
+def waitAndMove():
+    while True:
+        if len(dealList) != 0 and dealList[0]["moveAt"] < int(time.mktime(datetime.now().timetuple())):
+            dealList[0]["moveAt"] += 30
+            dealList.append(dealList.pop(0))
+        else:
+            time.sleep(2)
+
+def runListener():
+    app.run(host='127.0.0.1', port=8000, debug=True)
+dealList = []
+
+thread1 = Thread(target=waitAndMove).start()
+runListener()
 
 
-
-app.run(host='185.211.170.140', port=8000, debug=True)
+# 3) Подключиться к таблице
+# 4) Изменять данные в таблице
+# 5) Проверить на проде
